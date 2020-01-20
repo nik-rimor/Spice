661e55c3938ee1d80f76afa777750a3819fdf7bc
Nikos Kekes <nickkekes@gmail.com>
2020-01-20 11:04:13 +0200

    add extensions methods and viewmodel for SubCategory views

--- Spice/Spice/Data/Migrations/20200119205856_add_SubCategoryModel.Designer.cs
+++ Spice/Spice/Data/Migrations/20200119205856_add_SubCategoryModel.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Spice.Data;
 
 namespace Spice.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200119205856_add_SubCategoryModel")]
+    partial class add_SubCategoryModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Spice/Spice/ViewModels/SubCategoryAndCategoryViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Spice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.ViewModels
+{
+    public class SubCategoryAndCategoryViewModel
+    {
+        public IEnumerable<Category> CategoryList { get; set; }
+        public SubCategory SubCategory { get; set; }
+        public List<string> SubCategoryList { get; set; }
+        public string StatusMessage { get; set; }
+
+
+
+    }
+}
