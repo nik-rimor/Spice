8fbabe7afc86ed974591921fe6c9638e4c3c4ba8
Nikos Kekes <nickkekes@gmail.com>
2020-01-23 07:18:46 +0200

    intermidiate commit

--- Spice/Spice/Areas/Admin/Controllers/MenuItemController.cs
+++ Spice/Spice/Areas/Admin/Controllers/MenuItemController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Spice.Data;
+using Spice.ViewModels;
 
 namespace Spice.Areas.Admin.Controllers
 {
@@ -15,17 +16,30 @@ namespace Spice.Areas.Admin.Controllers
         private readonly ApplicationDbContext _db;
         private readonly IWebHostEnvironment _webHostEnvironment;
 
+        [BindProperty]
+        public MenuItemViewModel MenuItemVM { get; set; }
+
         public MenuItemController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
         {
             _db = db;
             _webHostEnvironment = webHostEnvironment;
+            MenuItemVM = new MenuItemViewModel()
+            {
+                Category = _db.Category,
+                MenuItem = new Models.MenuItem()
+            };
         }
-
 
         public async Task<IActionResult> Index()
         {
             var menuItems = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).ToListAsync();
             return View(menuItems);
+        }
+
+        //GET - Create
+        public IActionResult Create()
+        {
+            return View(MenuItemVM);
         }
     }
 }
