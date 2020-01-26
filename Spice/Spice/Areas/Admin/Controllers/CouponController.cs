6eacb3d1723ad2f2bd551019e12893a09cf15745
Nikos Kekes <nickkekes@gmail.com>
2020-01-26 12:11:49 +0200

    add Create action for CouponController

--- Spice/Spice/Areas/Admin/Controllers/CouponController.cs
+++ Spice/Spice/Areas/Admin/Controllers/CouponController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Spice.Data;
+using Spice.Models;
 
 namespace Spice.Areas.Admin.Controllers
 {
@@ -22,6 +24,42 @@ namespace Spice.Areas.Admin.Controllers
         {
             var coupons = await _db.Coupon.ToListAsync();
             return View(coupons);
+        }
+
+        // GET - Create
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        // POST - Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Coupon coupon)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(coupon);
+            }
+
+            var files = HttpContext.Request.Form.Files;
+            if(files.Count >0)
+            {
+                byte[] p1 = null;
+                using (var fs1 = files[0].OpenReadStream())
+                {
+                    using (var ms1 = new MemoryStream())
+                    {
+                        fs1.CopyTo(ms1);
+                        p1 = ms1.ToArray();
+                    }
+                }
+                coupon.Picture = p1;
+            }
+            _db.Coupon.Add(coupon);
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
